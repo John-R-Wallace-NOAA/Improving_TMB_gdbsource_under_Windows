86cde48b8ea2f530400d474b34a1282d2b61bc03
John Wallace <John.Wallace@noaa.gov>
2022-09-25 00:20:21 -0700

    Create MSY2_GDB_Test_Code.R

---
+++ R_and _Cpp/MSY2_GDB_Test_Code.R
@@ -0,0 +1,16 @@
+
+
+file <- 'simpleError.R'
+gdbscript <- tempfile()
+#  txt <- paste("unset environment\nset breakpoint pending on\nb abort\nrun --vanilla -f", file, "\nbt\n")
+Rterm_Path <- paste(rev(rev(strsplit(Sys.which('Rterm'), "\\\\")[[1]])[-1]), collapse = "\\")
+txt <- paste("cd ", Rterm_Path, "\nset breakpoint pending on\nb abort\nrun --vanilla -f", file, "\nbt\n")
+cat(txt, file = gdbscript)
+# cat("quit\n", file = gdbscript, append = TRUE)   # Consistently hangs without the 'quit'. Use <Esc> to exit the hang.
+(cmd <- paste0("gdb Rterm -x ", gdbscript))
+# cmd <- paste0("gdb ", Sys.which('Rterm')))
+# (cmd <- paste0("gdb ", 'Rterm'))
+file.show(gdbscript)  # Look at the commands in gdbscript temp file, if desired.
+# system(cmd)  
+cmd <- paste("start", cmd)   
+shell(cmd)
