08964b5d6bfc95b94d451788a48d3b9b7cb687f6
John Wallace <John.Wallace@noaa.gov>
2022-09-25 01:55:48 -0700

    Update gdb ver 10.2 from mingw64 ver 8.5.0 to rtools42.bat

--- R_and _Cpp/gdb ver 10.2 from mingw64 ver 8.5.0 to rtools42.bat
+++ R_and _Cpp/gdb ver 10.2 from mingw64 ver 8.5.0 to rtools42.bat
@@ -1,2 +1,3 @@
 
 robocopy . W:\rtools42\x86_64-w64-mingw32.static.posix\bin gdb.exe libboost_regex-mt-x64.dll libdl.dll libexpat-1.dll libgcc_s_seh-1.dll libgmp-10.dll libiconv-2.dll liblzma-5.dll libmpfr-6.dll libncurses6.dll libncursesw6.dll libreadline8.dll libsource-highlight-4.dll libstdc++-6.dll libwinpthread-1.dll mman-win.dll xxhash.dll zlib1.dll 
+
