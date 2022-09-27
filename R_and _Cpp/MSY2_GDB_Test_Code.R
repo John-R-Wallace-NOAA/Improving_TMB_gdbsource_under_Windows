b8da4dca4e4f059e0c55556d132c827d0b191013
John Wallace <John.Wallace@noaa.gov>
2022-09-26 19:52:05 -0700

    Update MSY2_GDB_Test_Code.R

--- R_and _Cpp/MSY2_GDB_Test_Code.R
+++ R_and _Cpp/MSY2_GDB_Test_Code.R
@@ -4,11 +4,12 @@
 # give (at least for me on 2 different systems) a backtrace with line number information when using the compile options given in 
 # the help for TMB::gdbsource().
 
-# The pwd path issue always puts the current working directory on the path of 'Rterm' even when using 'unset environment'. The only way
-# this worked for me, without the fix below, is to remove Sys.which() from 'Rterm' and manually set the working directory in R to 
-# Sys.which('Rterm') [e.g. C:/R/R-4.2.1/bin/x64].
+# The pwd path issue always puts the current working directory as a prefix on the path of 'Rterm' even when using 'unset environment':
+#     Error creating process /c/TMB_Debug/C:\R\R-42~1.1\bin\x64\Rterm.exe, (error 2).  
+# The only way this worked for me, without the fix below, is to remove Sys.which() from 'Rterm' and manually set the working 
+# directory in R to Sys.which('Rterm') [e.g. C:/R/R-4.2.1/bin/x64].
 
-# Note that in GDB, the pwd path is set with 'cd' but looked with 'pwd'.
+# Note that on the GDB command line, the pwd path is set with 'cd' but looked with 'pwd'.
 
 # Perhaps with the correct compile options and different commands to gdb a proper backtrace with line numbers could be achieved.
 
